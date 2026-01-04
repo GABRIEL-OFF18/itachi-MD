fe5772f1e56eb690019ef6d538248154399b476c
Ell Sarmiento 🦩 <sarmientodieg@gmail.com>
2026-01-04 16:14:00 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-global.owner = ['5492916450307', '5219921062586']
+global.owner = ['5492916450307']
 global.sessionName = 'Sessions/Owner'
 global.version = '^2.0'
 global.pairing_code = true
@@ -16,19 +16,6 @@ global.bot = {
   api: 'https://api.stellarwa.xyz',
   web: 'https://studio.stellarwa.xyz'
 }
-
-global.mods = [
-  '5492916450307', // Creador
-  '5218711426787', // Miguel
-  '5491139010575', // Asistencia
-  '5219921062586', // Stellar
-  '5216671548329', // Legna
-  '593968263524', // Liliana
-  '5492324347631', // Akane
-  '51901019299', // Angel
-  '5492916439595', // Sebastian
-  '5511998790420' // Tokito
-]
 
 global.msgglobal = '[Error: *TypeError*] fetch failed'
 globalThis.dev = 'ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴɪɢʜᴛ ʟɪɢʜᴛ ⚡︎'
